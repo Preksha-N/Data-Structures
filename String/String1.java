108b631740d43316d7015750efc88ffdd9be544a
Preksha Shetti <71238045+Preksha-N@users.noreply.github.com>
2022-08-22 23:24:13 +0530

    String addition

---
+++ String/String1.java
@@ -0,0 +1,16 @@
+import java.lang.String;
+import java.util.*;
+class  String1 {
+    public static void main(String[] args) {
+      
+      // create strings
+      String first = "Java";
+      String second = "Python";
+      String third = "JavaScript";
+  
+      // print strings
+      System.out.println(first);   
+      System.out.println(second); 
+      System.out.println(third);   
+    }
+  }
